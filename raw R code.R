8ce454b76b21fbc1d9628991466b24639c701bc8
yuryzablotski <yzablotski@icloud.com>
2022-12-02 12:23:50 +0100

    robust regression blog fix. the video link did not work

--- raw R code.R
+++ raw R code.R
@@ -1,275 +1,93 @@
+# 1. create data
 library(tidyverse)
-library(gtsummary)
-
-d <- trial %>% select(trt, age, grade, response)
-
-# 1. Summarize data
-tbl_summary(d, by = trt)
-
-d %>%
-  tbl_strata(
-    strata = grade, 
-    ~.x %>% 
-      tbl_summary(by = trt))
-
-# 2. Summarize statistical tests
-d %>% 
-  tbl_summary(by = trt) %>% 
-  add_p() %>% 
-  add_q() %>% 
-  add_overall() %>% 
-  add_n() %>% 
-  add_ci() %>% 
-  add_stat_label(
-    label = all_continuous() ~ "Median (IQR)") 
-
-trial %>%
-  tbl_summary(
-    by        = trt,
-    statistic = all_continuous()~"{mean} ({sd})") %>%
-  add_p(test  = list(
-    all_continuous()  ~ "t.test", 
-    all_categorical() ~ "fisher.test"))
-
-trial %>% 
-  tbl_summary(
-    by = trt,
-    statistic = list(
-      age    ~ "{mean} ({sd})",
-      marker ~ "{mean} ({min}, {p25}, {p75}, {max})",
-      stage  ~ "{n} / {N} ({p}%)")) %>% 
-  add_p(test = list(
-    age    ~ "t.test",
-    marker ~ "wilcox.test",
-    stage  ~ "fisher.test")) %>% 
-  separate_p_footnotes()
-
-trial_paired <-
-  trial %>%
-  select(trt, marker, response) %>%
-  group_by(trt) %>%
-  mutate(id = row_number()) %>%
-  ungroup()
-
-trial_paired %>%
-  filter(complete.cases(.)) %>%
-  group_by(id) %>%
-  filter(n() == 2) %>%
-  ungroup() %>%
-  tbl_summary(by = trt, include = -id) %>%
-  add_p(test = list(
-    marker   ~ "paired.t.test",
-    response ~ "mcnemar.test"), 
-    group    = id)
-
-trial %>%
-  tbl_summary(
-    by           = trt,
-    statistic    = age   ~ "{mean} ({sd})", 
-    label        = grade ~ "New Name - Tumor Grade",
-    digits       = all_continuous() ~ 1,
-    # missing    = "no", 
-    missing_text = "Missing values",
-    type         = list(response ~ "categorical",
-                        death    ~ "categorical"),
-    sort         = everything()  ~ "frequency", 
-    percent      = "cell", 
-    include      = - ttdeath
-  ) %>%
-  add_p(pvalue_fun = ~style_pvalue(.x, digits = 3))
-
-# 3. Summarize regression models
-bm <- arm::bayesglm(
-  response ~ trt + age + grade, 
-  data = trial, family = binomial)
-
-bm_table <- tbl_regression(bm, exponentiate = TRUE) 
-
-bm_table
-
-library(survival)
-cm <- coxph(
-  Surv(ttdeath, death) ~ trt + age + grade, 
-  data = trial)
-
-cm_table <- tbl_regression(cm, exponentiate = TRUE) 
-
-cm_table
-
-# several univariate models
-uvlm_table <- trial %>%
-  select(response, trt, age, grade) %>%
-  tbl_uvregression(
-    method       = glm,
-    y            = response,
-    method.args  = list(family = binomial),
-    exponentiate = TRUE
-  ) 
-
-uvlm_table
-
-uvcm_table <- tbl_uvregression(
-  trial %>% 
-    # could be 300+ predictors
-    select(ttdeath, death, trt, age, grade),
-  method       = coxph,
-  y            = Surv(ttdeath, death),
-  exponentiate = TRUE
-) 
-
-uvcm_table
-
-## Side-by-side Regression Models
-fancy_table <-
-  tbl_merge(
-    tbls        = list(bm_table, cm_table),
-    tab_spanner = c("Tumor Response","Time to Death")
-  )
-
-fancy_table
-
-uni_multi <- tbl_merge(
-  tbls        = list(
-    tbl_summary(d), uvlm_table, bm_table),
-  tab_spanner = c(
-    "**Describe**", 
-    "**Univariate Models**", 
-    "**Multivariate Model**")
+d <- tibble(
+  predictor = c(  1,   2,   3,  4,   5, 6, 7),
+  outcome   = c(1.5, 2.3, 2.8,  4.1, 5.3, 0, 6.8)
 )
 
-uni_multi
+# 2. plot normal and median regressions
+ggplot(d, aes(predictor, outcome))+
+  geom_point()+
+  geom_smooth(method = lm, se = F)+
+  geom_quantile(color = "red", quantiles = 0.5)
 
-glm(response ~ trt + age + marker + ttdeath + grade, 
-    data = trial, family = binomial) %>% 
-  tbl_regression(
-    #pvalue_fun   = ~style_pvalue(.x, digits = 3),
-    exponentiate = TRUE
-  ) %>% 
-  # add_* helpers
-  add_n(location = "level") %>%
-  add_nevent(location = "level") %>%  
-  add_global_p() %>%   
-  add_q() %>%        
-  add_significance_stars(
-    hide_p = F, hide_se = T, hide_ci = F) %>% 
-  add_vif() %>% 
-  # modify_* helpers
-  modify_header(label = "**Predictor**") %>% 
-  modify_caption("Table 1. Cool looking table!") %>% 
-  modify_footnote(
-    ci = "CI = Credible Intervals are incredible ;)", 
-    abbreviation = TRUE) %>%
-  sort_p() %>% 
-  # aesthetics helpers
-  bold_p(t = 0.10, q = TRUE) %>% 
-  bold_labels() %>%    #bold_levels() 
-  italicize_levels()   #italicize_labels() 
+# 3. model median based quantile regression
+# How do we know QR is relly better?
 
-trial %>%
-  tbl_cross(
-    row = stage,
-    col = trt,
-    percent = "cell", # or column, row, none
-    missing = "ifany", 
-  ) %>%
-  add_p()
+library(quantreg)
+mr <- rq(outcome ~ predictor, data = d, tau = .5)
+lr <- lm(outcome ~ predictor, data = d)
 
-# loading the api data set
-data(api, package = "survey")
+# AIC estimates the relative amount of information
+# lost by a given model: the less information a 
+# model loses, the higher the quality of that model.
 
-svy_apiclus1 <- 
-  survey::svydesign(
-    id = ~dnum, 
-    weights = ~pw, 
-    data = apiclus1, 
-    fpc = ~fpc
-  ) 
+AIC(lr, mr) # => the lower AIS the better
 
-svy_apiclus1 %>%
-  tbl_svysummary(
-    # stratify summary stats by the "both" column
-    by = both, 
-    # summarize a subset of the columns
-    include = c(api00, api99, both),
-    # adding labels to table
-    label = list(api00 ~ "API in 2000",
-                 api99 ~ "API in 1999")
-  ) %>%
-  add_p() %>%   # comparing values by "both" column
-  add_overall() %>%
-  # adding spanning header
-  modify_spanning_header(c("stat_1", "stat_2") ~ 
-                           "**Met Both Targets**")
+# Moreover, taking a wrong regression could cost you
+# an important discovery
 
-theme_gtsummary_compact()
+library(sjPlot) # I did a video on this 📦
+theme_set(theme_bw())
+plot_models(lr, mr, show.values = TRUE)
 
-library(ISLR)    # for Auto dataset
-m <- lm(mpg ~ origin * horsepower, data = Auto)
+# 5. model several quantiles
 
-tbl_regression(m) %>% 
-  modify_caption("Compact theme")
+# like a box-plot
+qm25 <- rq(outcome ~ predictor, data = d, tau = 0.25)
+qm50 <- rq(outcome ~ predictor, data = d, tau = 0.5)
+qm75 <- rq(outcome ~ predictor, data = d, tau = 0.75)
 
-reset_gtsummary_theme()
+plot_models(lr, qm25, qm50, qm75,
+            show.values = TRUE)
 
-library(gt)
-trial %>%
-  # create a gtsummary table
-  tbl_summary(by = trt) %>%
-  # convert from gtsummary object to gt object
-  as_gt() %>%
-  # modify with gt functions
-  tab_header("Table 1: Baseline Characteristics") %>% 
-  tab_spanner(
-    label = "Randomization Group",  
-    columns = starts_with("stat_")
-  ) %>% 
-  tab_options(
-    table.font.size = "small",
-    data_row.padding = px(1)) 
+AIC(lr, qm25, qm50, qm75)
 
-library(flextable)
+# 6. get model results in table form
+qm <- rq(outcome ~ predictor, data = d, 
+         tau = seq(.25, .75, by = 0.25))
+library(gtsummary) # I did a video on this 📦
+tbl_regression(qm, se="nid", level = 0.95)
 
-fancy_table %>%
-  as_flex_table() %>% 
-  save_as_image(path = "fancy_table.png")
+summary(qm, se="nid") %>% plot()
 
-fancy_table %>%
-  as_flex_table() %>%
-  save_as_docx(path = "fancy_table.docx") 
+# 7. model a lot of quantiles in a real data
+data("engel")
+qm <- rq(foodexp ~ income, data = engel, 
+         tau = seq(.05, .95, by = 0.05))  # median regression
+
+ggplot(engel, aes(income, foodexp))+
+  geom_point()+
+  geom_quantile(color = "red", 
+                quantiles = seq(.05, .95, by = 0.05))
+
+
+summary(qm, se="nid") %>% plot()
+
+
+# like a box-plot
+lr   <- lm(foodexp ~ income, data = engel)
+qm05 <- rq(foodexp ~ income, data = engel, tau = 0.05)
+qm25 <- rq(foodexp ~ income, data = engel, tau = 0.25)
+qm50 <- rq(foodexp ~ income, data = engel, tau = 0.5)
+qm75 <- rq(foodexp ~ income, data = engel, tau = 0.75)
+qm95 <- rq(foodexp ~ income, data = engel, tau = 0.95)
+
+AIC(lr, qm05, qm25, qm50, qm75, qm95)
+
+plot_models(lr, qm05, qm25, qm50, qm75, qm95,
+            show.values = TRUE)
+
+# ?
+library(performance) # I did a video on this 📦
+compare_performance(
+  lr, qm05, qm25, qm50, qm75, qm95, 
+  rank = T)
 
 
 
-theme_gtsummary_compact()
 
-compact_table <- glm(response ~ trt + age + grade, 
-    data = trial, family = binomial) %>% 
-  tbl_regression(
-    #pvalue_fun   = ~style_pvalue(.x, digits = 3),
-    exponentiate = TRUE
-  ) %>% 
-  # add_* helpers
-  add_n(location = "level") %>%
-  add_nevent(location = "level") %>%  
-  add_global_p() %>%   
-  add_q() %>%        
-  add_significance_stars(
-    hide_p = F, hide_se = T, hide_ci = F) %>% 
-  add_vif() %>% 
-  # modify_* helpers
-  modify_header(label = "**Predictor**") %>% 
-  modify_caption("Table 1. Cool looking table!") %>% 
-  modify_footnote(
-    ci = "CI = Credible Intervals are incredible ;)", 
-    abbreviation = TRUE) %>%
-  sort_p() %>% 
-  # aesthetics helpers
-  bold_p(t = 0.10, q = TRUE) %>% 
-  bold_labels() %>%    #bold_levels() 
-  italicize_levels()   #italicize_labels() 
 
-compact_table %>%
-  as_flex_table() %>% 
-  save_as_image(path = "compact_table.png")
 
-reset_gtsummary_theme()
+
 
